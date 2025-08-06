525fa9b310f088d94b0ace314e5da5167845031a
Nouaar <nourallah.maamar@esprit.tn>
2025-08-06 12:30:57 +0100

    navbar and footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import React, { useState } from "react";
-import Login from "./auth/Login";
+import React from "react";
 
 export default function Home() {
-
-
- return (<Login />);}
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+      <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-800">
+        Welcome to the Vehicle Management System
+      </h1>
+    </div>
+  );
+}
