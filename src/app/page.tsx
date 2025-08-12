05ac6cfab751d95c050140158971e28dc31f36f5
youssef-bs <youssefbensaid827@gmail.com>
2025-08-12 10:05:28 +0100

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-"use client";
+  "use client";
 
-import React from "react";
-import Fiche_Entretien from "./vehicles/maintenance/page";
+  import React from "react";
+  import Slide from "@/components/slide/Slide";
+  import ProtectedRoute from "@/components/ProtectedRoute";
 
-export default function Home() {
-  return (<div
-      className="min-h-screen bg-cover bg-center flex items-center justify-center"
-      style={{
-        backgroundImage: 'url("/images/background_cars.jpg")', 
-      }}
-    >
-     
-    </div>
-  );
-}
+  export default function Home() {
+    return (<>
+    <ProtectedRoute>
+      <Slide />
+    </ProtectedRoute>
+    
+    </>
+    );
+  }
--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -2,13 +2,20 @@
 
 import Link from "next/link";
 import { Home, Info, LogOut } from "lucide-react"; 
+import { useAuth } from "@/context/AuthContext";
 
 export default function Navbar() {
+  const { logout } = useAuth();
+
+  const handleLogout = async () => {
+    await logout();
+  }
+
+
   return (
     <header className="bg-white shadow-md">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         
-        {/* Logo */}
         <Link href="/" className="text-xl font-bold text-blue-600">
           <img
             src="/images/Logo_paeb.jpg"
@@ -17,7 +24,6 @@ export default function Navbar() {
           />
         </Link>
 
-        {/* Navigation */}
         <nav className="space-x-6 flex items-center">
           <Link href="/" className="flex items-center gap-2 text-gray-700 hover:text-blue-500">
             <Home size={18} />
@@ -27,10 +33,10 @@ export default function Navbar() {
             <Info size={18} />
             À propos
           </Link>
-          <Link href="/logout" className="flex items-center gap-2 text-gray-700 hover:text-blue-500">
+          <p className="flex items-center gap-2 text-gray-700 hover:text-blue-500" onClick={handleLogout}>
             <LogOut size={18} />
             Déconnexion
-          </Link>
+          </p>
         </nav>
       </div>
     </header>
