f159821d78914b0faf3180875534298baa1b1ea5
Maamar NourAllah <nourallah.maamar@esprit.tn>
2025-08-14 12:25:21 +0100

    add or update form for vehicles added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@
   import React from "react";
   import Slide from "@/components/slide/Slide";
   import ProtectedRoute from "@/components/ProtectedRoute";
-import MaintenanceForm from "./vehicles/maintenance/page";
+import MaintenanceForm from "./vehicles/maintenance/Add_Update/[id]/page";
+import VehiculeForm from "./vehicles/Add_Update/[id]/VehiculeForm";
 
   export default function Home() {
     return (<>
     <ProtectedRoute>
       <Slide />
-      <MaintenanceForm />
+      <VehiculeForm/>
     </ProtectedRoute>
     
     </>
